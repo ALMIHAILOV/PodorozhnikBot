3cfdfd2c0507450e0c5c775070773580f7a3d72c
Alexander Mikhailov <mihailov_a@hotmail.com>
2021-03-15 17:29:47 +0300

    Add App

--- src/main/java/org/Boton/App.java
+++ src/main/java/org/Boton/App.java
@@ -1,13 +1,20 @@
 package org.Boton;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.Boton.services.Bot;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.generics.LongPollingBot;
+
+public class App {
+    public static void main( String[] args ) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot((LongPollingBot) new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
     }
 }
+
