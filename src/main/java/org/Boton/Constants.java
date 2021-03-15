c28eb178df51a3cbc9807e8fd0d0a5fed943eeb3
Alexander Mikhailov <mihailov_a@hotmail.com>
2021-03-15 16:09:34 +0300

    Add Constants and UserService

--- src/main/java/org/Boton/Constants.java
+++ src/main/java/org/Boton/Constants.java
@@ -2,7 +2,7 @@ package org.Boton;
 
 public final class Constants {
     //Telegram bot credentials
-    public static final String BOT_USERNAME = "";
-    public static final String BOT_TOKEN = "";
+    public static final String BOT_USERNAME = "1614431601:AAFMB7ptBOLowGEdZ7rx5BGWHW9rEtTuDNI";
+    public static final String BOT_TOKEN = "PodorozhnikTelegramBot";
     public static final int CREATOR_ID = 352081004;
 }
