8c02dd0a79b12b465c0082863a6ab0067003f4f1
mikhailov <mihailov_a@hotmail.com>
2021-03-23 16:57:45 +0300

    for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: java -cp target/classes/org/Boton:target/dependency/* App
--- src/main/java/org/PodorozhnikBot/utils/HibernateSessionFactoryUtil.java
+++ src/main/java/org/PodorozhnikBot/utils/HibernateSessionFactoryUtil.java
@@ -6,6 +6,9 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HibernateSessionFactoryUtil {
     private static SessionFactory sessionFactory;
 
@@ -13,12 +16,22 @@ public class HibernateSessionFactoryUtil {
     }
 
     public static SessionFactory getSessionFactory() {
+
+
         if (sessionFactory == null) {
             try {
+                Map<String,String> jdbcUrlSettings = new HashMap<>();
+                String jdbcDbUrl = System.getenv("JDBC_DATABASE_URL");
+                if (null != jdbcDbUrl) {
+                    jdbcUrlSettings.put("hibernate.connection.url", System.getenv("JDBC_DATABASE_URL"));
+                }
+
                 Configuration configuration = new Configuration().configure();
                 configuration.addAnnotatedClass(User.class);
                 configuration.addAnnotatedClass(Statistic.class);
-                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().
+                        configure("hibernate.cfg.xml").
+                        applySettings(jdbcUrlSettings);
                 sessionFactory = configuration.buildSessionFactory(builder.build());
 
             } catch (Exception e) {
